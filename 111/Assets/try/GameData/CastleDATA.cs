c0f55154b7eff8050ebe55f07b3c89520a15ef64
Frédéric Lin <frederic.lin@epita.fr>
2023-04-14 00:59:10 +0200

    flo we need to talk about namespace

--- 111/Assets/try/GameData/CastleDATA.cs
+++ 111/Assets/try/GameData/CastleDATA.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace UnityRoyale
+
+[CreateAssetMenu(fileName = "Castle", menuName = "BeforeDown/Castle")]
+public class CastleDATA : ScriptableObject
 {
-    [CreateAssetMenu(fileName = "Castle", menuName = "BeforeDown/Castle")]
-    public class CastleDATA : ScriptableObject
-    {
-        public float Health = 15f;
-        public float damage = 3f;
-        public float AttackRange = 5f;
-    }
+    public float Health = 15f;
+    public float damage = 3f;
+    public float AttackRange = 5f;
+    public float fireRate = 1f;
+
+    public GameObject projectilePrefab;
 }
+
