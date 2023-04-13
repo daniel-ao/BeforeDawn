d48760526f8925a1a2445e3bd69c892974d713a1
Frédéric Lin <frederic.lin@epita.fr>
2023-04-13 23:56:06 +0200

    gonna change soon

---
+++ 111/Assets/Try-yinpi/Turret2/new turret/Turret.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Turret : MonoBehaviour
+{
+
+    public TurretData turretData;
+    public Vector3 popo;
+    private Transform target;
+    private float fireCountdown = 0f;
+
+
+
+    void Update()
+    {
+        FindTarget();
+        if (target != null)
+        {
+            if (fireCountdown <= 0f)
+            {
+                Fire();
+                fireCountdown = 1f / turretData.fireRate;
+            }
+            fireCountdown -= Time.deltaTime;
+        }
+    }
+
+    void FindTarget()
+    {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag("Enemy");
+        float closestDistance = Mathf.Infinity;
+        Transform closestEnemy = null;
+        foreach (GameObject enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < closestDistance)
+            {
+                closestDistance = distanceToEnemy;
+                closestEnemy = enemy.transform;
+            }
+        }
+        if (closestEnemy != null && closestDistance <= turretData.range)
+        {
+            target = closestEnemy;
+        }
+        else
+        {
+            target = null;
+        }
+    }
+
+    void Fire()
+    {
+        GameObject projectile = Instantiate(turretData.projectilePrefab, transform.position + popo, Quaternion.identity) as GameObject;
+        Projectile script = projectile.GetComponent<Projectile>();
+        script.target = target;
+        script.damage = turretData.damage;
+    }
+}
