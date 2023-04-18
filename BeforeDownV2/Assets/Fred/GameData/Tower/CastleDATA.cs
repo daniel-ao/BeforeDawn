8927799d7b3f1f976fe0429bfeb2126f8eb6fa49
Frédéric Lin <frederic.lin@epita.fr>
2023-04-18 14:28:10 +0200

    unit, tower bug corrected

--- BeforeDownV2/Assets/Fred/GameData/Tower/CastleDATA.cs
+++ BeforeDownV2/Assets/Fred/GameData/Tower/CastleDATA.cs
@@ -7,7 +7,8 @@ public class CastleDATA : ScriptableObject
 {
     public float Health = 15f;
     public float damage = 3f;
-    public float AttackRange = 5f; 
+    public float AttackRange = 5f;
     public float fireRate = 1f;
+    public float SpawningRate = 0.2f;
     public GameObject projectilePrefab;
 }
