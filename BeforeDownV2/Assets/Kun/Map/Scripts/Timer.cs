f61fad4d7bfa77d65651f61958534f525a0a308e
kun.li <kun.li@epita.fr>
2023-05-24 15:11:26 +0200

    update Map

---
+++ BeforeDownV2/Assets/Kun/Map/Scripts/Timer.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace UnityRoyale
+{
+    public class Timer : MonoBehaviour
+    {
+        [SerializeField] Text TimeText;
+        [SerializeField] GameObject Win;
+        [SerializeField] GameObject Lose;
+
+        float time;
+
+        void FixedUpdate()
+        {
+
+            if (Win.activeSelf || Lose.activeSelf)
+            {
+                return;
+            }
+
+            else
+            {
+                time += Time.fixedDeltaTime;
+                TimeText.text = System.TimeSpan.FromSeconds(time).ToString(@"mm\:ss\:ff");
+            }
+            
+        }
+    }
+}
