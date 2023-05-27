9f8365de2abde75afc9f2f2900a502a32c6e1e34
kun.li <kun.li@epita.fr>
2023-05-27 12:25:44 +0200

    update all

---
+++ BeforeDownV2/Assets/QuitGame.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UnityRoyale
+{
+    public class QuitGame : MonoBehaviour
+    {
+        public void QuitGames()
+        {
+            Application.Quit();
+        }
+    }
+}
