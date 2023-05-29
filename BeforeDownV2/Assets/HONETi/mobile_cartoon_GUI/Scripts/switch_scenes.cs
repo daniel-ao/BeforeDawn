7b32922f22298ad59ddbe686e1216d3810943751
kun.li <kun.li@epita.fr>
2023-05-29 13:42:44 +0200

    addLoadingSceneAndResolvErros

--- BeforeDownV2/Assets/HONETi/mobile_cartoon_GUI/Scripts/switch_scenes.cs
+++ BeforeDownV2/Assets/HONETi/mobile_cartoon_GUI/Scripts/switch_scenes.cs
@@ -1,13 +1,16 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 using System.Collections;
+using UnityEngine.SceneManagement;
 
 public class switch_scenes : MonoBehaviour {
 	
 	public string sceneName = "";
 
-	// Use this for initialization
-	void Start () {
+   
+
+    // Use this for initialization
+    void Start () {
 		Button b = GetComponent<Button> ();
 		if (b != null && sceneName != "")
 		{
--- BeforeDownV2/Assets/Kun/Gacha/Scripts/ChangeScene.cs
+++ BeforeDownV2/Assets/Kun/Gacha/Scripts/ChangeScene.cs
@@ -8,8 +8,32 @@ public class ChangeScene : MonoBehaviour
 {
 
     public string sceneName;
+    public int nb = 0;
+
     public void ChnageS()
     {
-        SceneManager.LoadScene(sceneName);
+        SceneManager.LoadScene(sceneName,LoadSceneMode.Single);
     }
+
+
+    public void ReloadSB()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex-1,LoadSceneMode.Single);
+    }
+
+    public void ReloadS()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex,LoadSceneMode.Single);
+    }
+
+    public void LoadNextS()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex+1,LoadSceneMode.Single);
+    }
+
+    public void LoadS()
+    {
+        SceneManager.LoadScene(nb,LoadSceneMode.Single);
+    }
+
 }
